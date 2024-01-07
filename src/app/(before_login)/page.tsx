ef84ac3f500a306183085d7785a1a8b93c88525c
jjanmo <jjanmo07@hanmail.net>
2024-01-07 11:53:26 +0900

    feat: add tailwindcss

--- src/app/(before_login)/page.tsx
+++ src/app/(before_login)/page.tsx
@@ -5,9 +5,6 @@ import Link from 'next/link';
 const Main = () => {
   return (
     <div className={styles.container}>
-      <h1>H1 - Hello World</h1>
-      <h2>H2 - Hello World</h2>
-      <h3>H3 - Hello World</h3>
       <div className={styles.left}>
         <Logo />
       </div>
